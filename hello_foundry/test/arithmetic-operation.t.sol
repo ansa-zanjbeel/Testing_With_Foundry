15095146939a5c753f928a174775b9e369527297
ansa-zanjbeel <ansazanjbeel.5@gmail.com>
2024-01-12 17:43:12 +0500

    commit

--- hello_foundry/test/arithmetic-operation.t.sol
+++ hello_foundry/test/arithmetic-operation.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/arithmetic-operation.sol";
+import "../src/2-arithmetic-operation.sol";
 
 contract ArithmeticOperationTest is Test {
     ArithmeticOperation public arithmeticoperation;
