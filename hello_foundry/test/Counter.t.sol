7396947b3e04fcd2cf5c9b5a9d599cf7e7b902b6
ansa-zanjbeel <ansazanjbeel.5@gmail.com>
2024-01-13 21:36:32 +0500

    commit

--- hello_foundry/test/Counter.t.sol
+++ hello_foundry/test/Counter.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/1-Counter.sol";
+import "../src/1-counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
---
+++ hello_foundry/test/error.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/3-error.sol";
+
+contract ErrorTest is Test {
+    Error public error;
+
+    function setup() public {
+        error = new Error();
+    }
+
+    function testFail() public {
+        error.throwError();
+    }
+}
